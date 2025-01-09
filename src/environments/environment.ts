d5de4e9156382769c31d5daff8b38283d8d3b230
lengyelnorbi <58424631+lengyelnorbi@users.noreply.github.com>
2025-01-09 20:24:40 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
     projectId: 'pwa-task-management-app',
     appId: '1:257806859048:web:5894ca66328c4f937b6e1d',
     storageBucket: 'pwa-task-management-app.appspot.com',
-    apiKey: 'AIzaSyDb8UHN9O2QOoaJ1--kyE9y3CdXyxQhR8s',
+    apiKey: '',
     authDomain: 'pwa-task-management-app.firebaseapp.com',
     messagingSenderId: '257806859048',
     measurementId: 'G-Z67MB2T2WC',
